70f38bbcd807fb79ffd399b03908d106fb310fbc
unknown <janf@gmatc.matc.edu>
2022-10-07 08:56:50 -0500

    Completed Activity 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,14 +9,14 @@ import { Component, OnInit } from '@angular/core';
 
 export class AppComponent implements OnInit{
   title = 'client';
-  weatherForecasts: any;
+  posts: any;
 
   constructor (private http: HttpClient){
 
   }
   ngOnInit():void{
-    this.http.get('http://localhost:5072/weatherforecast').subscribe(
-      response => {this.weatherForecasts = response;},
+    this.http.get('http://localhost:5072/api/posts').subscribe(
+      response => {this.posts = response;},
       error => {console.log(error)}
     );
   }
